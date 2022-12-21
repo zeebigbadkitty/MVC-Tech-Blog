567ecb97064b27e9b52e1ff1164f18305c5f5191
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-21 14:23:46 -0600

    Index for the models started.

--- models/index.js
+++ models/index.js
@@ -1,1 +1,16 @@
 //has many relations, etc.so like user.hasMany posts / user.hasMany comments / post.belongsTo user / post.hasMany comments / comments.belongsTo user / comments.belongsTo posts
+
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Post, Comment };
