b11816caa0df329cacc61a204b13f4c5ec255342
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-23 20:59:11 -0600

    Still working on controllers.

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -15,24 +15,24 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const commentData = await Comment.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
+// router.delete('/:id', withAuth, async (req, res) => {
+//   try {
+//     const commentData = await Comment.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
 
-    if (!commentData) {
-      res.status(404).json({ message: 'No comment found with this id!' });
-      return;
-    }
+//     if (!commentData) {
+//       res.status(404).json({ message: 'No comment found with this id!' });
+//       return;
+//     }
 
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.status(200).json(commentData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
