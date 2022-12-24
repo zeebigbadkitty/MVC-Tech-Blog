ccc248b80f7ae0be1a80b4ea38aca85b03b99ea2
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-23 20:54:03 -0600

    Working on homeroutes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
 const postRoutes = require('./postRoutes');
+const withAuth = require('../../utils/auth');
 
 router.use('/user', userRoutes);
 router.use('/comment', commentRoutes);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,12 +4,12 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all posts and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['name'], //May not need?
         },
       ],
     });
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('main', { 
-      posts, 
+      posts, //...?
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -33,15 +33,19 @@ router.get('/post:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['name'],//??
+        },
+        {
+          model: Comment,
+          include: [User]
         },
       ],
     });
 
-    const posts = postData.get({ plain: true });
+    const post = postData.get({ plain: true });
 
-    res.render('project', {
-      ...posts,
+    res.render('single-post', { //placeholder for now (create new handlebar view)
+      post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -49,30 +53,11 @@ router.get('/post:id', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/main');
     return;
   }
 
