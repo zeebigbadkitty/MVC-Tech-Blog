bde1395aa379d3af97f4c7f6ae282dfa0dcd646e
Socially Awkward Streams <candice.radam@gmail.com>
2023-01-19 15:26:27 -0600

    Got the main/blog page to render, and got the seeds to work. Just working on getting comments to render.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,16 +11,20 @@ router.get('/', async (req, res) => {
           model: User,
           attributes: ['username'], 
         },
+        {
+          model: Comment,
+          attributes: ['body'],
+        },
       ],
     });
 
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
-
+    console.log(posts)
     // Pass serialized data and session flag into template
-    res.render('login', { 
-      posts, //...?
-      logged_in: req.session.logged_in 
+    res.render('blog', { 
+      posts, 
+      // logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
@@ -33,7 +37,7 @@ router.get('/post:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],//??
+          attributes: ['username'],//??
         },
         {
           model: Comment,
